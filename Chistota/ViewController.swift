264c39e8d8a3b4b83a8ed3f1ebf414a4f10ffb3e
Ed <sinyakov.gsg@gmail.com>
2020-03-01 19:16:49 +0300

    Initial Commit

---
+++ Chistota/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chistota
+//
+//  Created by Eduard Sinyakov on 3/1/20.
+//  Copyright © 2020 Eduard Siniakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
